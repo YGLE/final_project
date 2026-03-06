07310075b4bad68daeb0ce2b4e6d09633566a164
YGLE <yann.glever@orange.com>
2026-03-06 17:39:45 +0100

    add error management

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,19 @@
-import requests # import requests library
+import requests  # import requests library
 
-# define emotion_detector function that take a test in imput
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' # url of the EmotionPredict service
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} # set the headers for the API request
-    myobj = { "raw document": { "text": text_to_analyze } } # create a dictionary with the text in input
-    response = requests.post(url, json = myobj, headers=header) # send a POST request to the API service
-    return response.text
+# define emotion_detector function that takes a text input
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the EmotionPredict service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers for the API request
+    myobj = {
+        "raw document": {
+            "text": text_to_analyse
+        }
+    }  # Create a dictionary with the text in input
+
+    try:
+        response = requests.post(url, json=myobj, headers=header)  # Send a POST request to the API service
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        return response.text  # Return the API response
+    except requests.exceptions.RequestException as e:
+        return f"Error: {e}"
+    
